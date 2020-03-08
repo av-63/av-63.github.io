ab2ce210eb71e2a18d817415efd779ebb9fb3b64
av-63 <60980757+av-63@users.noreply.github.com>
2020-03-08 20:49:03 +0300

    Update smart.php

--- 40in50/Yrok/src/mailer/smart.php
+++ 40in50/Yrok/src/mailer/smart.php
@@ -13,7 +13,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers МОЖНО ЧЕРЕЗ ЯШУ smtp.yandex.ru но измени строку 18 (там сейчас 465)
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'temp_bot_sm@mail.ru';                 // Наш логин
-$mail->Password = 'NewSM1983';                           // Наш пароль от ящика
+$mail->Password = 'Password';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to ДЛЯ MAIL 465, для яши загугли...
  
