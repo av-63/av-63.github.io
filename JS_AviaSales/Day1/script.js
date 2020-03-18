f59156d8dcd89556ba06400d5d526012caf94fb0
av-63 <60980757+av-63@users.noreply.github.com>
2020-03-18 10:39:55 +0300

    Воркшоп по JS
    Первый день + все ДЗ

---
+++ JS_AviaSales/Day1/script.js
@@ -0,0 +1,50 @@
+const formSearch = document.querySelector('.form-search'),
+    inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__date-depart');
+
+const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керчь', 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеринбург', 'Одесса', 'Ухань', 'Шымкент', 'Нижний Новгород', 'Калининград', 'Вроцлав', 'Ростов-на-Дону'];
+
+const showCity = (input, list) => {
+    list.textContent = '';
+
+    if (input.value !== '') {
+        const filterCity = city.filter((item) => {
+            const fixItem = item.toLowerCase();
+            return fixItem.includes(input.value.toLowerCase());
+        });
+
+        filterCity.forEach((item) => {
+            const li = document.createElement('li');
+            li.classList.add('dropdown__city');
+            li.textContent = item;
+            list.append(li);
+        });
+    }
+};
+
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesFrom.value = target.textContent;
+        dropdownCitiesFrom.textContent = '';
+    };
+});
+
+inputCitiesTo.addEventListener('input', () => {
+    showCity(inputCitiesTo, dropdownCitiesTo);
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesTo.value = target.textContent;
+        dropdownCitiesTo.textContent = '';
+    };
+});
